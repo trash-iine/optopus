f8697920ca08758c71a9a57190c928b29e09a5ce
Trash-iine <50480656+trash-iine@users.noreply.github.com>
2026-03-17 22:14:31 +0900

    FEAT: use benchmark struct in main

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,21 @@
 use optopus::{
-    heuristic::{
-        BreakoutLocalSearchForMaxCut, Heuristic, ParallelHeuristic, StopCondition, TabuSearch,
+    benchmark::{
+        Benchmark, BenchmarkSetting, BreakoutLocalSearchSetting, MaxCutBenchmarkSetting,
+        MaxCutHeuristicSetting,
     },
-    problem::{MaxCutFlipNeighbor, MaxCutSwapNeighbor, max_cut::MaxCut},
-    search_state::SearchState,
+    heuristic::StopCondition,
+    problem::max_cut::MaxCut,
 };
-use serde::Serialize;
-
-#[derive(Serialize)]
-struct BenchmarkResult {
-    status: String,
-    instance: String,
-    best_objective: f64,
-    best_iteration: u64,
-    time_taken: f64,
-    solution: Vec<usize>,
-}
-
-#[derive(Serialize)]
-struct Benchmark {
-    results: Vec<BenchmarkResult>,
-}
 
 fn main() {
     tracing_subscriber::fmt::init();
 
-    let mut results = Vec::new();
+    let mut benchmark = Benchmark::new();
 
     let files = glob::glob("data/max_cut/G*").unwrap();
-    // let files = glob::glob("data/max_cut/G1").unwrap();
     for file in files {
-        let instance_number = file
-            .as_ref()
-            .unwrap()
+        let path = file.unwrap();
+        let instance_number = path
             .file_name()
             .unwrap()
             .to_str()
@@ -44,66 +27,28 @@ fn main() {
             continue;
         }
 
-        tracing::info!("Processing file: {:?}", file);
-        let mc = MaxCut::load_from_file(file.as_ref().unwrap().to_str().unwrap()).unwrap();
-        let mut state = SearchState::new(&mc, rand::rng());
-        let sc = StopCondition::new(Some(10_000_000), None, None);
-        // let sc = StopCondition::new(Some(30000), None, None);
-        let bls = BreakoutLocalSearchForMaxCut::new(
-            (3, (mc.len() / 10) as u64),
-            sc,
-            1000,
-            (mc.len() / 100) as u64,
-            0.8,
-            0.5,
-        );
-        let ts = TabuSearch::<MaxCutFlipNeighbor>::new(
-            // StopCondition::new(Some(200000), None, None),
-            StopCondition::new(Some(1000000), None, None),
-            (3, (mc.len() / 100) as u64),
-            None,
-        );
-        // let status = bls.run(&mut state);
-        let start = std::time::Instant::now();
-        // let status = ts.run(&mut state);
-        let status = bls.run(&mut state);
-        let end = std::time::Instant::now();
-        tracing::info!(
-            "Best objective: {} ({:?}, {:?}), time taken: {:?}",
-            state.best_solution.objective,
-            state.best_iteration,
-            state.best_time - state.start_time,
-            end - start
-        );
-        results.push(BenchmarkResult {
-            status: {
-                match status {
-                    Ok(_) => "success".to_string(),
-                    Err(e) => format!("error: {}", e),
-                }
-            },
-            instance: file
-                .unwrap()
-                .file_name()
-                .unwrap()
-                .to_str()
-                .unwrap()
-                .to_string(),
-            best_objective: state.best_solution.objective.into(),
-            best_iteration: state.best_iteration,
-            time_taken: state.best_time.elapsed().as_secs_f64(),
-            solution: state
-                .best_solution
-                .cut
-                .iter()
-                .filter(|(_, v)| **v)
-                .map(|(&i, _)| i)
-                .collect(),
+        let instance_path = path.to_str().unwrap().to_string();
+        tracing::info!("Processing file: {}", instance_path);
+
+        let mc = MaxCut::load_from_file(&instance_path).unwrap();
+        let n = mc.len();
+
+        let setting = BenchmarkSetting::MaxCut(MaxCutBenchmarkSetting {
+            instance_path,
+            heuristic: MaxCutHeuristicSetting::BreakoutLocalSearch(BreakoutLocalSearchSetting {
+                tabu_tenure: (3, (n / 10) as u64),
+                t: 1000,
+                l0: (n / 100) as u64,
+                p0: 0.8,
+                q: 0.5,
+            }),
+            stop_condition: StopCondition::new(Some(10_000_000), None, None),
         });
+
+        benchmark.run(setting);
     }
 
-    // Serialize results to TOML
-    let toml_str = toml::to_string(&Benchmark { results }).unwrap();
+    let toml_str = toml::to_string(&benchmark.results).unwrap();
     let output_file = chrono::Local::now()
         .format("result/%Y%m%d_%H%M%S.toml")
         .to_string();
