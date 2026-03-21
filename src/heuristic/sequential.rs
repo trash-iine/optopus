fb4eaf0964777e8d23d6219422b7cd012542d601
Trash-iine <50480656+trash-iine@users.noreply.github.com>
2026-03-21 20:47:19 +0900

    DOC: modify sequential heuristic module

--- src/heuristic/sequential.rs
+++ src/heuristic/sequential.rs
@@ -2,12 +2,19 @@ use super::Heuristic;
 use crate::error::OptError;
 use crate::search_state::{ProblemTrait, SearchStateCloneType};
 
+/// Sequential meta-heuristic that runs a list of heuristics one after another.
+///
+/// Each sub-heuristic operates on a fresh clone of the current state
+/// (using [`SearchStateCloneType::ClearBest`]).
+/// After each sub-heuristic completes, its results are merged back into the main state.
+/// The outer stopping condition is checked between sub-heuristic runs.
 pub struct Sequential<Problem: ProblemTrait> {
     pub stop_condition: super::StopCondition,
     pub heuristics: Vec<Box<dyn Heuristic<Problem>>>,
 }
 
 impl<'a, Problem: ProblemTrait> Sequential<Problem> {
+    /// Create a new [`Sequential`] heuristic with the given stopping condition and list of heuristics.
     pub fn new(
         stop_condition: super::StopCondition,
         heuristics: Vec<Box<dyn Heuristic<Problem>>>,
@@ -16,6 +23,11 @@ impl<'a, Problem: ProblemTrait> Sequential<Problem> {
             stop_condition,
             heuristics,
         }
+    }
+
+    /// Add a heuristic to the last of the sequence.
+    pub fn push_heuristic(&mut self, heuristic: Box<dyn Heuristic<Problem>>) {
+        self.heuristics.push(heuristic);
     }
 }
 
