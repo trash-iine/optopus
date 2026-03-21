ed19a619f523e6acab1ea42a109f86c5636ac435
Trash-iine <50480656+trash-iine@users.noreply.github.com>
2026-03-22 05:09:24 +0900

    DOC: modify qubo module description

--- src/problem/qubo/mod.rs
+++ src/problem/qubo/mod.rs
@@ -1,3 +1,7 @@
+//! Quadratic Unconstrained Binary Optimization (QUBO) problem definition and neighborhood structures.
+//!
+//! QUBO minimizes the energy `E(x) = Σ Q[i][j] * x[i] * x[j]` over binary variables `x ∈ {0,1}^n`.
+
 mod neighbor;
 mod problem;
 
