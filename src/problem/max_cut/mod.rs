62db7884cc7de8f0de2bdc1d81a0a5838a32bd38
Trash-iine <50480656+trash-iine@users.noreply.github.com>
2026-03-22 03:01:12 +0900

    DOC: modify max cut module description

--- src/problem/max_cut/mod.rs
+++ src/problem/max_cut/mod.rs
@@ -1,3 +1,8 @@
+//! Maximum Cut (MaxCut) problem definition and neighborhood structures.
+//!
+//! Given an undirected weighted graph, MaxCut seeks a partition of the vertices into
+//! two sets that maximizes the total weight of edges crossing the partition.
+
 mod neighbor;
 mod problem;
 
