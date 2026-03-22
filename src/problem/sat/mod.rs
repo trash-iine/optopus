2fa34ec837bcf8a95412a86ea834328fb9550337
Trash-iine <50480656+trash-iine@users.noreply.github.com>
2026-03-22 19:00:50 +0900

    DOC: modify sat module description

--- src/problem/sat/mod.rs
+++ src/problem/sat/mod.rs
@@ -1,5 +1,10 @@
-mod definition;
-mod neighbor;
+//! Maximum Satisfiability (MaxSAT) problem definition and neighborhood structures.
+//!
+//! Given a CNF formula in DIMACS format, MaxSAT seeks an assignment of boolean variables
+//! that maximizes the number of satisfied clauses.
 
-pub use definition::{Sat, SatSolution};
+mod neighbor;
+mod problem;
+
 pub use neighbor::{SatFlipNeighbor, SatSwapNeighbor};
+pub use problem::{Sat, SatSolution};
